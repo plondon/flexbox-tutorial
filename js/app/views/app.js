05e18d4c0097f55504e203afd1f7a37b5f570a39
Phil London <philip.n.london@gmail.com>
2015-06-23 09:05:49 -0400

    prevent default on anchor click and trigger app router

--- js/app/views/app.js
+++ js/app/views/app.js
@@ -17,10 +17,22 @@ var AppView = Backbone.View.extend({
 	render: function($temp) {
 		this.$content.html($temp);
 		_.delay(_.bind(function() {
+
 			this.$content.addClass('active');
 			$('body').removeClass('loading');
+
 		}, this), 100);
 	},
+	events: {
+		'click a:not(a.outside-link)': function (e) {
+		  if ($(e.currentTarget).attr('target') === '_blank') { return; }
+
+		  var href = e.currentTarget.href.replace(window.location.origin, '');
+
+		  window.appRouter.navigate(href, {trigger: true});
+		  e.preventDefault();
+		}
+	}
 });
 
 return AppView;
