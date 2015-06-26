63c9672aca2ea10ea753a45a86fedf9aaa02a0fa
Phil London <plondon@gm.slc.edu>
2015-06-26 12:46:52 -0400

    add global prefixes with bourbon instead of grunt-postcss

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,18 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 
+		// postcss: {
+		//   options: {
+		//     processors: [
+		//       require('autoprefixer-core')({browsers: 'last 2 versions'}), // add vendor prefixes
+		//       require('cssnano')() // minify the result
+		//     ]
+		//   },
+		//   dist: {
+		//     src: 'css/*.css'
+		//   }
+		// },
+
 		sass: {
 			dev: {
 				options: {
@@ -33,5 +45,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-shell-spawn');
+	// grunt.loadNpmTasks('grunt-postcss');
+
 	grunt.registerTask('default', ['shell:server', 'watch']);
 };
