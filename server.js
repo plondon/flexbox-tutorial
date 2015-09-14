982ed6ac59be8dfe77b8ef637f902d46f194f064
Phil London <plondon@gm.slc.edu>
2015-09-14 16:55:03 -0400

    clean up server

--- server.js
+++ server.js
@@ -5,19 +5,12 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-// app.set('view engine', 'ejs');
-// app.set('view engine', 'html');
+// set the view engine to ejs but use html
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname);
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/'));
-
-// set the home page route
-// app.get('/', function(req, res) {
-//   res.render('index.html');
-// });
 
 app.get('*', function(req, res) {
   res.render('index.html');
