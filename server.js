1fa6bebbb955847316027789bc2fe4a9021ab8a1
Phil London <plondon@gm.slc.edu>
2015-09-23 09:27:43 -0400

    change comment on server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 // set the port of our application
-// process.env.PORT lets the port be set by Heroku
+// process.env.PORT lets the port be set by third party
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs but use html
